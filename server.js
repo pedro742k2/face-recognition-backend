1db0ebc3fc04097f13899dba3596c9aa049dea08
pedro742k2 <pmpb742k2@gmail.com>
2021-01-10 19:47:16 +0000

    Changing routes menu

--- server.js
+++ server.js
@@ -14,22 +14,12 @@ const getUsers = require("./Controllers/GetUsers");
 const db = knex({
   client: "pg",
   connection: {
-    connectionString: process.env.DATABASE_URL,
+    conne: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
   },
 });
-
-let error;
-
-db.select("*")
-  .from("users")
-  .then((data) => {
-    console.log("db ->", data);
-    error = data;
-  })
-  .catch(console.log);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -40,15 +30,13 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.json({
     msg: "Hey, you're on the root",
-    availableOptions: [
+    availableRoutes: [
       "/get_users",
       "/signin",
       "/register",
       "/profile/:id",
       "/image",
       "/image_url",
-      `error: ${error}`,
-      `process.env.DATABASE_URL: ${process.env.DATABASE_URL}`,
     ],
   });
 });
