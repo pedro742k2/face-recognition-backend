088f8517967e90cab4b4ff01a1251320ab94c95c
Pedro Batista <pmpb752k2@gmail.com>
2022-03-27 14:55:12 +0100

    Fixed connectionString spelling mistake

--- src/routes.js
+++ src/routes.js
@@ -30,7 +30,7 @@ redisClient.set("key", "value");
 const db = knex({
   client: "pg",
   connection: {
-    connectString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
       rejectUnauthorized: false,
     },
